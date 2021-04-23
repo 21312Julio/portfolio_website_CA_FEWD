fb7791ae4f7d351a9c2a8b8d6357e830e5d99d17
julio <21312@student.dorset-college.ie>
2021-04-23 01:56:12 +0100

    php file change

--- php/email.php
+++ php/email.php
@@ -16,11 +16,6 @@ $header = "From:julio.beyeler@gmail.com"."\r\n".
             "Reply-To:".$email."\r\n".
                 "X=Mailer:PHP/".phpversion();
 
-if(mail($to,$subject,$body,$header)) {
-    
-}else{
-    echo("Error sending Email.");
-}
 
 }
 
